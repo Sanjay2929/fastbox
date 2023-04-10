13a2ad798c596528f7eb88e9ecfb567c51baaa45
Ankit-191 <godaraankit003>
2023-04-10 13:48:47 +0530

    update

---
+++ src/components/Ourspecialties.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+function Ourspecialties() {
+  return (
+    <div>
+        <Container/>
+        <Row/>
+        <Col/>
+
+        <Container>
+            <Row>
+                <Col>
+                    <p></p>
+                </Col>
+            </Row>
+        </Container>
+    </div>
+  )
+}
+
+export default Ourspecialties
