8deeaff64044dc0d48e799a44b99f905fada766f
Ankit-191 <godaraankit003>
2023-04-10 13:54:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Ourspecialties from './components/Ourspecialties';
