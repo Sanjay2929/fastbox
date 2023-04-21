3a15b3db119590d1ea00aacf3c2a449b92bb8b94
Ankit-191 <godaraankit003>
2023-04-21 10:07:35 +0530

    commit

--- src/Components/Gps.jsx
+++ src/Components/Gps.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import map from '../Assets/img/png/map.webp'
+import Greenbutton from './Greenbutton'
 
 const Gps = () => {
   return (
@@ -13,7 +14,9 @@ const Gps = () => {
                 <Col lg={7} className=' py-5'>
                         <p className='ff_Anybody fw_700 fsize_48 clr_dark_green'>Follow your shipment via GPS</p>
                         <p className='ff_Read_Hat_text fw_400 fsize_22 clr_light_gray w_810 pt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate..</p>
-                        <button className='ff_Poppins fw_400 fsize_22 border-0 bg_green clr_white btncal mt-5'>Learn More</button>
+                        <div className='mt-5'>
+                            <Greenbutton Green_text="Learn More"/>
+                        </div>
                 </Col>
                 <Col lg={5}>
                     <div className='position_absolute h-100 w_50  top-0'>
